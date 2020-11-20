084c291bc8f3134b836086ec62d37f52e6f3b953
HamzaIjaz1 <iamhamzza@gmail.com>
2020-11-20 23:37:57 +0500

    fixing

--- model/taskModel.js
+++ model/taskModel.js
@@ -2,40 +2,36 @@ var LocalStorage = require("node-localstorage").LocalStorage;
 const localStorage = new LocalStorage("./task");
 
 function getLocal() {
-  const data = JSON.parse(localStorage.getItem("task"));
+    const data = localStorage.getItem("task");
 
-  if (data) {
-    return data;
-  } else {
-    return [];
-  }
+    if (data) {
+        return JSON.parse(data);
+    } else {
+        return [];
+    }
 }
 
-module.exports.get_all = function (user) {
-  return new Promise(function (resolve, reject) {
-    var tasks = [...getLocal()];
+module.exports.get_all = function(user) {
+    return new Promise(function(resolve, reject) {
+        var tasks = [...getLocal()];
 
-    if (tasks.length > 0) {
-      resolve(tasks);
-    } else {
-      reject("User Not Found");
-    }
-  });
+        if (tasks.length > 0) {
+            resolve(tasks);
+        } else {
+            reject("User Not Found");
+        }
+    });
 };
 
+module.exports.task_model = function(task) {
+    return new Promise(function(resolve, reject) {
+        let newtask = {...task };
+        newtask.id = Math.random();
 
-module.exports.task_model = function (task) {
+        var tasks = [...getLocal()];
 
-    return new Promise(function (resolve, reject) {
-    let newtask = {...task};
-    newtask.id = Math.random();
-
-   var tasks = [...getLocal()];
-
-    
-    tasks.push(newtask);
-    localStorage.setItem("task", JSON.stringify(tasks));
-    resolve(newtask);
-    
-  })
-}
+        tasks.push(newtask);
+        localStorage.setItem("task", JSON.stringify(tasks));
+        resolve(newtask);
+    });
+};
--- model/userModel.js
+++ model/userModel.js
@@ -2,95 +2,95 @@ var LocalStorage = require("node-localstorage").LocalStorage;
 const localStorage = new LocalStorage("./scratch");
 
 function getLocal() {
-  const data = JSON.parse(localStorage.getItem("abcdeee"));
+    const data = localStorage.getItem("abcdeee");
 
-  if (data) {
-    return data;
-  } else {
-    return [];
-  }
+    if (data) {
+        return JSON.parse(data);
+    } else {
+        return [];
+    }
 }
 
-module.exports.signup_user_model = function (user) {
+module.exports.signup_user_model = function(user) {
 
-    return new Promise(function (resolve, reject) {
-    let newUser = {...user};
-    newUser.id = Math.random();
+    return new Promise(function(resolve, reject) {
+        let newUser = {...user };
+        newUser.id = Math.random();
 
-    let found=false;
-   var users = [...getLocal()];
+        let found = false;
+        var users = [...getLocal()];
 
-    //   localStorage.setItem('abcdeee',JSON.stringify(user))
-    if (users.length > 0) {
-      users.forEach((item) => {
-        if (item.email === user.email) {
-            found=true;
-        } else {
-          
+        //   localStorage.setItem('abcdeee',JSON.stringify(user))
+        if (users.length > 0) {
+            users.forEach((item) => {
+                if (item.email === user.email) {
+                    found = true;
+                } else {
+
+                }
+            });
         }
-      });
-    }
-if (found){
-    reject("User already Exists");
-}else{
-    users.push(newUser);
-    localStorage.setItem("abcdeee", JSON.stringify(users));
-    resolve({id:newUser.id,email:newUser.email});
-}
-    
-  });
-};
+        if (found) {
+            reject("User already Exists");
+        } else {
+            users.push(newUser);
+            localStorage.setItem("abcdeee", JSON.stringify(users));
+            resolve({ id: newUser.id, email: newUser.email });
+        }
 
-module.exports.signin_user_model = function (user) {
-  console.log(user);
-
-  return new Promise(function (resolve, reject) {
-    
-    var users = [...getLocal()];
-
-    //   localStorage.setItem('abcdeee',JSON.stringify(user))
-    let found = false;
-    if (users.length > 0) {
-      users.forEach((item) => {
-        if (item.email === user.email) {
-          found = true;
-        } 
-      });
-    }
-
-    if (found){
-        resolve(user.email)
-    }else{
-        reject('User Not Found')
-    }
-
-  });
-};
-
-module.exports.get_user_model = function (user) {
-  
-    return new Promise(function (resolve, reject) {
-      
-      var users = [...getLocal()];
-  
-      //   localStorage.setItem('abcdeee',JSON.stringify(user))
-      let found = false;
-      let r;
-      if (users.length > 0) {
-          r = users.filter(item=>(item.email === user))
-        // users.forEach((item) => {
-        //   if (item.email === user.email) {
-        //     found = true;
-        //   } 
-        // });
-        console.log('rrrr ', r)
-      }
-      delete r[0].password;
-      if (r.length>0){
-          resolve(r[0])
-      }else{
-          reject('User Not Found')
-      }
-  
     });
-  };
+};
+
+module.exports.signin_user_model = function(user) {
+    console.log(user);
+
+    return new Promise(function(resolve, reject) {
+
+        var users = [...getLocal()];
+
+        //   localStorage.setItem('abcdeee',JSON.stringify(user))
+        let found = false;
+        if (users.length > 0) {
+            users.forEach((item) => {
+                if (item.email === user.email) {
+                    found = true;
+                }
+            });
+        }
+
+        if (found) {
+            resolve(user.email)
+        } else {
+            reject('User Not Found')
+        }
+
+    });
+};
+
+module.exports.get_user_model = function(user) {
+
+    return new Promise(function(resolve, reject) {
+
+        var users = [...getLocal()];
+
+        //   localStorage.setItem('abcdeee',JSON.stringify(user))
+        let found = false;
+        let r;
+        if (users.length > 0) {
+            r = users.filter(item => (item.email === user))
+                // users.forEach((item) => {
+                //   if (item.email === user.email) {
+                //     found = true;
+                //   } 
+                // });
+            console.log('rrrr ', r)
+        }
+        delete r[0].password;
+        if (r.length > 0) {
+            resolve(r[0])
+        } else {
+            reject('User Not Found')
+        }
+
+    });
+};
